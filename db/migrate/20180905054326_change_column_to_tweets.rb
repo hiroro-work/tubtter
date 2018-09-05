32758949da15e12f50ece778f90738b5ef22a8a6
hiroro-work <hiroro.work@gmail.com>
2018-09-05 14:56:45 +0900

    カード全体をリンク化 & DB修正

--- db/migrate/20180905054326_change_column_to_tweets.rb
+++ db/migrate/20180905054326_change_column_to_tweets.rb
@@ -1,5 +1,5 @@
 class ChangeColumnToTweets < ActiveRecord::Migration[5.2]
   def change
-    change_column :tweets, :content, :text, null: false, default: ''
+    change_column :tweets, :content, :text, null: false, default: nil
   end
 end
